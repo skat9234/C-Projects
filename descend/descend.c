498e51d449c38c595c764ae6e869f36016ee0fc6
Neha Palan Kamat <shreyaskamat13@gmail.com>
2022-10-20 10:13:58 +0530

    descending order

---
+++ descend/descend.c
@@ -0,0 +1,33 @@
+#include <stdio.h>  
+  
+int main(void) {  
+    int number[10];
+ 
+        int i, j, a, n;
+        printf("Enter the value of Number of terms: \n");
+        scanf("%d", &n);
+ 
+        printf("Enter the numbers: ");
+        for (i = 0; i < n; i++)
+	        scanf("%d", &number[i]);
+ 
+        for (i = 0; i < n; i++) 
+        {
+            for (j = i + 1; j < n; j++) 
+            {
+                if (number[i] < number[j]) 
+                {
+                    a = number[i];
+                    number[i] = number[j];
+                    number[j] = a;
+                }
+            }
+        }
+ 
+        printf("The numbers arranged in descending order are given below\n");
+ 
+        for (i = 0; i < n; i++) 
+        {
+            printf("%d\n", number[i]);
+        }
+}  
