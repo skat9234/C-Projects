326d09585e08d4102876b43073e7e10ff1fe8609
Neha Palan Kamat <shreyaskamat13@gmail.com>
2023-01-21 15:59:12 +0530

    FIX

--- Code/symmetry/symmetry.c
+++ Code/symmetry/symmetry.c
@@ -39,18 +39,3 @@ int main(void) {
     }
     
 }
-
-// So then, he told me to add more batches and he told me to give him
-// Some time (a few classes) and its really nice thing you've built,
-// Give me some time and I'll speak to you further on this
-// then??
-// He asked "How much time did it take you to build this?"
-// I said "It took me 2 weeks to design and 4 weeks to develop,
-// so 1 1/2 months in total"
-// I thought of this a month back...
-// I designed the look of the app and the logo in 2 weeks
-// I gave it 30-35 min everyday (In the BUS!!) and CS class smtimes
-// Instead of sleeping in the bus, I used to sleep at home and waste time
-// Hutt u gonna be the one givin me a job!
-// u're getting more mks than me in the aryaans tests!!!!!!!
-// ull go to IIT 
